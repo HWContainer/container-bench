76d52be90a6956f939fce45a104004aad54dd7d7
nkuacac <xiawencan@huawei.com>
2021-01-18 16:01:55 +0800

    out l7 test

--- script2/out_l7_nodeport.sh
+++ script2/out_l7_nodeport.sh
@@ -1,33 +1,38 @@
-
-podserver_name=`kubectl get pods --selector app=$server -o jsonpath='{.items[0].metadata.name}'`
-podserver_ip=`kubectl get pods --selector app=$server -o jsonpath='{.items[0].status.podIP}'`
+sleep 60
+nodes=`kubectl get pods -l app=$server -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
 nodeserver_ip=`kubectl get pods --selector app=$server -o jsonpath='{.items[0].status.hostIP}'`
 
 svc_port=`kubectl get svc $service -ojsonpath='{.spec.ports[0].nodePort}'`
 
 currentTimeStamp=`date +%s.%2N`
 sleep 60
-python query_csv.py $prometheus_url $currentTimeStamp $nodeserver_ip
-python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
+for node in $nodes; do
+python query_csv.py $prometheus_url $currentTimeStamp $node
+done
 
 currentTimeStamp=`date +%s.%2N`
+if [[ "$nodec_ip" == "127.0.0.1" ]]; then
+fortio load -qps $qps -c $connect -t 120s --keepalive=false http://$nodeserver_ip:$svc_port
+else
 sshpass -p Huawei@123 ssh -oStrictHostKeyChecking=no root@$nodec_ip<<END
-./fortio load -qps $qps -c $connect -t 120s --keepalive=false http://$nodeserver_ip:$svc_port
+fortio load -qps $qps -c $connect -t 120s --keepalive=false http://$nodeserver_ip:$svc_port
 END
+fi
 
-sleep 30
-
-python query_csv.py $prometheus_url $currentTimeStamp $nodeserver_ip
-python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
+for node in $nodes; do
+python query_csv.py $prometheus_url $currentTimeStamp $node
+done
+sleep 60
 
 currentTimeStamp=`date +%s.%2N`
+if [[ $nodec_ip == "127.0.0.1" ]]; then
+fortio load -qps $qps -c $connect -t 120s http://$nodeserver_ip:$svc_port
+else
 sshpass -p Huawei@123 ssh -oStrictHostKeyChecking=no root@$nodec_ip<<END
-./fortio load -qps $qps -c $connect -t 120s http://$nodeserver_ip:$svc_port
+fortio load -qps $qps -c $connect -t 120s http://$nodeserver_ip:$svc_port
 END
+fi
 
-sleep 30
-
-python query_csv.py $prometheus_url $currentTimeStamp $nodeserver_ip
-python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
-
-
+for node in $nodes; do
+python query_csv.py $prometheus_url $currentTimeStamp $node
+done
