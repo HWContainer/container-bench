ea84125ce8b2d069d82726f6aa6404125ad34ac0
nkuacac <xiawencan@huawei.com>
2021-02-08 21:04:10 +0800

    skip clean when diff test

--- script/prepare_vm.sh
+++ script/prepare_vm.sh
@@ -4,6 +4,8 @@ process_image=$3
 
 yum install -y docker
 service docker restart
+docker rm -vf $(docker ps -aq)
+docker volume rm $(docker volume ls -qf dangling=true)
 docker run -d -v /sys:/host/sys -v /proc:/host/proc --network host $node_image --web.listen-address=0.0.0.0:32101 --path.procfs=/host/proc --path.sysfs=/host/sys --collector.filesystem.ignored-mount-points='^/(dev|proc|sys|var/lib/docker/.+)($|/)' --collector.filesystem.ignored-fs-types='^(autofs|binfmt_misc|cgroup|configfs|debugfs|devpts|devtmpfs|fusectl|hugetlbfs|mqueue|overlay|proc|procfs|pstore|rpc_pipefs|securityfs|sysfs|tracefs)$'
 
 docker run --entrypoint="" -d -v /proc:/host/proc --network host $process_image /bin/process-exporter -config.path=/etc/process-name.yaml -procfs=/host/proc --web.listen-address=0.0.0.0:32256
