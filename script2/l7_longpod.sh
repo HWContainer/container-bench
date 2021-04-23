0ce7524120153cf41a660d142748a515dbc356e6
nkuacac <xiawencan@huawei.com>
2021-04-23 15:57:00 +0800

    add ns

--- script2/l7_longpod.sh
+++ script2/l7_longpod.sh
@@ -1,11 +1,13 @@
 
-podserver_name=`kubectl get pods --selector app=$server -o jsonpath='{.items[0].metadata.name}'`
-podserver_ip=`kubectl get pods --selector app=$server -o jsonpath='{.items[0].status.podIP}'`
-nodeserver_ip=`kubectl get pods --selector app=$server -o jsonpath='{.items[0].status.hostIP}'`
+ns=${namespace:-default}
 
-podsclient_name=`kubectl get pods -l app=$client -ojsonpath="{range .items[*]}{..metadata.name}{' '}{end}"|tr ' ' '\n'|sort|uniq`
-nodesclient_ip=`kubectl get pods -l app=$client -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
+podserver_ip=`kubectl get pods -n $ns --selector app=$server -o jsonpath='{.items[0].status.podIP}'`
+nodeserver_ip=`kubectl get pods -n $ns -l app=$server -o jsonpath='{.items[0].status.hostIP}'`
 
+podsclient_name=`kubectl get pods -n $ns -l app=$client -ojsonpath="{range .items[*]}{..metadata.name}{' '}{end}"|tr ' ' '\n'|sort|uniq`
+nodesclient_ip=`kubectl get pods -n $ns -l app=$client -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
+
+if [[ -z "${FAST}" ]]; then
 sleep 60
 currentTimeStamp=`date +%s.%2N`
 sleep 60
@@ -14,12 +16,12 @@ python query_csv.py $prometheus_url $currentTimeStamp $nodeserver_ip
 for nodeclient_ip in $nodesclient_ip; do
 python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
 done
-
+fi
 currentTimeStamp=`date +%s.%2N`
 
 for podclient_name in $podsclient_name; do
 {
-kubectl exec $podclient_name -- /usr/bin/fortio load -qps $qps -c $connect -t 120s  http://$podserver_ip > $podclient_name.log 2>&1
+kubectl exec $podclient_name -n $ns -- /usr/bin/fortio load -qps $qps -c $connect -t 120s  http://$podserver_ip > $podclient_name.log 2>&1
 } &
 done
 wait
@@ -27,7 +29,7 @@ wait
 for podclient_name in $podsclient_name; do
 cat $podclient_name.log
 done
-
+if [[ -z "${FAST}" ]]; then
 sleep 30
 
 python query_csv.py $prometheus_url $currentTimeStamp $nodeserver_ip
@@ -35,3 +37,4 @@ python query_csv.py $prometheus_url $currentTimeStamp $nodeserver_ip
 for nodeclient_ip in $nodesclient_ip; do
 python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
 done
+fi
--- script2/l7_shortsvc.sh
+++ script2/l7_shortsvc.sh
@@ -1,10 +1,12 @@
+ns=${namespace:-default}
+svc=${service:-server-perf-1}
+svc_ip=`kubectl get svc -n $ns $svc -o jsonpath='{..clusterIP}'`
+nodesserver_ip=`kubectl get pods -n $ns -l app=$server -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
 
-svc_ip=`kubectl get svc server-perf-1 -o jsonpath='{..clusterIP}'`
-nodesserver_ip=`kubectl get pods -l app=$server -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
+podsclient_name=`kubectl get pods -n $ns -l app=$client -ojsonpath="{range .items[*]}{..metadata.name}{' '}{end}"|tr ' ' '\n'|sort|uniq`
+nodesclient_ip=`kubectl get pods -n $ns -l app=$client -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
 
-podsclient_name=`kubectl get pods -l app=$client -ojsonpath="{range .items[*]}{..metadata.name}{' '}{end}"|tr ' ' '\n'|sort|uniq`
-nodesclient_ip=`kubectl get pods -l app=$client -ojsonpath="{range .items[*]}{..hostIP}{' '}{end}"|tr ' ' '\n'|sort|uniq`
-
+if [[ -z "${FAST}" ]]; then
 sleep 60
 currentTimeStamp=`date +%s.%2N`
 sleep 60
@@ -14,17 +16,18 @@ done
 for nodeclient_ip in $nodesclient_ip; do
 python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
 done
-
+fi
 currentTimeStamp=`date +%s.%2N`
 for podclient_name in $podsclient_name; do
 {
-kubectl exec $podclient_name -- /usr/bin/fortio load -qps $qps -c $connect -t 120s --keepalive=false http://$svc_ip > $podclient_name.log 2>&1
+kubectl exec $podclient_name -n $ns -- /usr/bin/fortio load -qps $qps -c $connect -t 120s --keepalive=false http://$svc_ip > $podclient_name.log 2>&1
 } &
 done
 wait
 for podclient_name in $podsclient_name; do
 cat $podclient_name.log
 done
+if [[ -z "${FAST}" ]]; then
 sleep 30
 
 for nodeserver_ip in $nodesserver_ip; do
@@ -33,4 +36,4 @@ done
 for nodeclient_ip in $nodesclient_ip; do
 python query_csv.py $prometheus_url $currentTimeStamp $nodeclient_ip
 done
-
+fi
